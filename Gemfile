dc37c0a84a00f2605ab0421fb75116485eefc5ed
Filipe Fasolin <ffasolin@gmail.com>
2017-05-03 11:59:52 +0100

    Cleans database before tests

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.4.0'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'capybara'
-gem 'rspec'
+
 gem 'sinatra'
 gem 'rake'
+group :test do
+  gem 'database_cleaner'
+  gem 'rspec'
+end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,10 +1,9 @@
 feature 'home' do
   scenario 'user sees list of links' do
-    visit '/links'
-    Link.create(
-    :url => "https://www.theguardian.com/uk",
-    :title => "Guardian",
-    )
+    visit '/links/new'
+    fill_in('title', with: 'Guardian')
+    fill_in('url', with: 'https://www.theguardian.com/uk')
+    click_button 'Submit'
     expect(page).to have_content 'https://www.theguardian.com/uk'
   end
 end
